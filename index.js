52e4df961244127fa9d1e4b9c6b845eb1f7e85aa
Anh-vu <anhuvdinh@gmail.com>
2021-08-12 09:40:03 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
+let x;
+
+function superbowlWin(record){
+    x = record.find(({result}) => result === 'W');
+    if(x === undefined){
+        console.log(x);
+        return;
+    }else if(x.year === "1969"){
+        console.log(x);
+        console.log(x.year);
+        return x.year;
+    }
+}
+
+
 
